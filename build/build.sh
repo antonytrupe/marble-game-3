b66ce9e18a25476ad7b47656b323ed5b4dc654e1
TonyT <antony.trupe@gmail.com>
2025-12-04 18:41:44 -0500

    update build script

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pwd
+# pwd
 
 # Get the path to the script itself, handling various execution methods and symlinks
 SCRIPT_PATH=$(readlink -f "$0")
@@ -11,39 +11,57 @@ SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 # Change to the script's directory (optional, but useful if your script needs local access)
 cd "$SCRIPT_DIR"
 
-pwd
-
-# This script assumes you have set the environment variable GODOT_PATH in your system
-# Environment variables on Windows are accessed via $VAR_NAME in bash/git bash
+##################
 EXPORT_NAME="Windows Desktop"
 
 # Make sure the directory exists (mkdir -p works in bash/mingw)
 mkdir -p ./godot/exports/win-64
 
 OUTPUT_PATH=$(realpath "./godot/exports/win-64/marblegame.exe")
-echo "Output path: $OUTPUT_PATH"
 
-echo "Starting Godot export..."
-
-# echo godot-mono --headless --export-release "$EXPORT_NAME" "$OUTPUT_PATH"
-
+echo "Starting Godot Windows export..."
 
 # Execute the command using the environment variable
 godot-mono --path ../ --headless --export-release "$EXPORT_NAME" "$OUTPUT_PATH"
 
-pwd
-
 # Immediately check the exit status stored in $?
 if [ $? -eq 0 ]; then
-    echo "✅ Godot build SUCCEEDED!"
+    echo "✅ Godot Windows build SUCCEEDED!"
     # Continue with deployment steps (e.g., upload to SteamCMD)
 else
-    echo "❌ Godot build FAILED!"
+    echo "❌ Godot Windows build FAILED!"
     # Stop the script or handle the error
     exit 1
 fi
 
-echo "Godot Build complete."
+echo "Godot Windows Build complete."
+##################
+
+##################
+EXPORT_NAME="Linux"
+
+# Make sure the directory exists (mkdir -p works in bash/mingw)
+mkdir -p ./godot/exports/linux-x64
+
+OUTPUT_PATH=$(realpath "./godot/exports/linux-x64/marblegame.x86_64")
+
+echo "Starting Godot Linux export..."
+
+# Execute the command using the environment variable
+godot-mono --path ../ --headless --export-release "$EXPORT_NAME" "$OUTPUT_PATH"
+
+# Immediately check the exit status stored in $?
+if [ $? -eq 0 ]; then
+    echo "✅ Godot Linux build SUCCEEDED!"
+    # Continue with deployment steps (e.g., upload to SteamCMD)
+else
+    echo "❌ Godot Linux build FAILED!"
+    # Stop the script or handle the error
+    exit 1
+fi
+
+echo "Godot Linux Build complete."
+##################
 
 echo "Starting Steam build..."
 
