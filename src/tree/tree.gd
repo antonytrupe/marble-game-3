584019b1f3555a3960584e576a7278cf07912718
TonyT <antony.trupe@gmail.com>
2026-01-12 20:29:07 -0500

    refactor MarbleTree class; enhance age management and apple growth logic; update scene structure for improved organization

--- src/tree/tree.gd
+++ src/tree/tree.gd
@@ -1,25 +1,62 @@
 class_name MarbleTree
 extends StaticBody3D
 
-#50 years
-@export var maturity: int = int(60 * 60 * 24 * 360 * (50.0))
+const APPLE_SCENE = preload("res://src/apple/apple.tscn")
 
-##milliseconds
-@export var age: float = 0
+@export var maturity: int = int(60 * 60 * 24 * 360 * (8))
+@export var age: MarbleAge = MarbleAge.new()
 @export var warp_speed: float = 1
-
-#@onready var world = $/root/Game/World
-#@onready var ageLabel = %AgeLabel
+var turn = 0
 @onready var warp_detector: WarpDetector = $WarpDetectorArea3D
 @onready var label_3d: Label3D = $Label3D
 
-var turn = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	var r = float(age.age) / float(maturity)
+	var s = clampf(r, .001, 1.0)
+	scale = Vector3(s, s, s)
+	label_3d.text = "Age:%.f\nWarp:%.f\nScale:%.2f" % [age.age, warp_speed, scale.x]
+
+
+#delta is in seconds
+func _physics_process(delta: float):
+	if is_server():
+		age.age += delta * warp_speed
+	var _turn: int = age.age / 6 + 1
+	for i in range(self.turn, _turn):
+		_start_turn(i)
+	self.turn = _turn
+
+
+func _start_turn(_turn):
+	#print('starting turn %.f'%turn)
+	#if the tree mature, then it can grow apples
+	if age.age > maturity:
+		#apples start growing in March
+		if age.get_month() == 0:
+			var i = randi_range(1, 200)
+			if i == 1:
+				print("add apple")
+				_add_apple()
+
+
+func _add_apple():
+	var x = randf()
+	var y = randf()
+	var z = randf()
+	var l = sqrt(x * x + y * y + z * z)
+	var apple: Apple = APPLE_SCENE.instantiate()
+	apple.position = Vector3(x / l, y / l, z / l)
+
+func is_server() -> bool:
+	return multiplayer.is_server()
 
 
 func get_data():
 	var save_dict = {
 		"transform": var_to_str(transform),
-		"age": age,
+		"age": age.age,
 		"turn": turn,
 		"warp_speed": warp_speed,
 	}
@@ -29,12 +66,11 @@ func get_data():
 func load_node(node_data):
 	#transform = str_to_var(node_data["transform"])
 	if "age" in node_data:
-		age = node_data.age
+		age.age = node_data.age
 	if "warp_speed" in node_data:
 		warp_speed = node_data.warp_speed
 	if "turn" in node_data:
 		turn = node_data.turn
-
 
 func calculate_warp():
 	var closest: WarpMonument = null
@@ -48,23 +84,3 @@ func calculate_warp():
 		warp_speed = closest.warp_speed
 	else:
 		warp_speed = 1
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	var r = float(age) / float(maturity)
-	#print(r)
-	var s = clampf(r, .001, 1.0)
-	#print(s)
-	scale = Vector3(s, s, s)
-	label_3d.text = "Age:%.f\nWarp:%.f\nScale:%.2f" % [age, warp_speed, scale.x]
-
-
-#delta is in seconds
-func _physics_process(delta: float):
-	if is_server():
-		age = age + delta * warp_speed
-
-
-func is_server() -> bool:
-	return multiplayer.is_server()
