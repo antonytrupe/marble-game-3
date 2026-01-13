a90f63eb0f4f86b8c1e35267ccee59943b51b614
TonyT <antony.trupe@gmail.com>
2026-01-12 20:27:25 -0500

    add MarbleAge class; implement age tracking functionality with placeholder methods

---
+++ src/age/age.gd
@@ -0,0 +1,14 @@
+class_name MarbleAge
+extends Resource
+
+## seconds since creation, taking warp bubbles into account
+@export var age: float = 0
+
+func get_month():
+	return 0
+
+func get_day_of_month():
+	return 0
+
+func get_years():
+	return 0
