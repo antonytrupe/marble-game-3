244c31fdd25f5e8e0076c6a048dbe5d9af69ff9b
TonyT <antony.trupe@gmail.com>
2026-01-13 19:42:49 -0500

    feat: update Apple and Tree nodes to use RigidBody3D and enhance collision properties

--- src/apple/apple.gd
+++ src/apple/apple.gd
@@ -1,16 +1,17 @@
 #@tool
 class_name Apple
-extends Node3D
+extends RigidBody3D
 
 #apples take 150 days to mature
 ##days
-@export var maturity: int = int(60 * 60 * 24 * (150))
+@export var maturity: int = int(MarbleAge.SECONDS_IN_DAY * (150))
 @export var age: MarbleAge = MarbleAge.new()
 @export var warp_speed: float = 1
 
 var turn = 0
 
 @onready var warp_detector: WarpDetector = $WarpDetectorArea3D
+@onready var flora = $/root/Game/World/Flora
 
 func _process(_delta: float) -> void:
 	pass
@@ -22,10 +23,18 @@ func _process(_delta: float) -> void:
 func _physics_process(delta: float):
 	if is_server():
 		age.age += delta * warp_speed
-	var _turn: int = age.age / 6 + 1
+	@warning_ignore("narrowing_conversion")
+	var _turn: int = age.age / MarbleAge.SECONDS_IN_TURN + 1
 	for i in range(self.turn, _turn):
 		_start_turn(i)
 	self.turn = _turn
+
+	if freeze and age.age > maturity:
+		# pass
+#		turn on physics and let it drop to the ground
+		freeze = false
+		freeze_mode = RigidBody3D.FREEZE_MODE_KINEMATIC
+		# reparent(flora)
 
 
 func is_server() -> bool:
@@ -35,3 +44,36 @@ func is_server() -> bool:
 func _start_turn(_turn):
 	#print('starting turn %.f'%turn)
 	pass
+
+func calculate_warp():
+	var closest: WarpMonument = null
+	#var closest_distance=0
+	for w: WarpMonument in warp_detector.warp_monuments.values():
+		var distance = w.position.distance_to(position)
+		if !closest or distance < closest.position.distance_to(position):
+			closest = w
+			#closest_distance=distance
+	if closest:
+		warp_speed = closest.warp_speed
+	else:
+		warp_speed = 1
+
+
+func get_data():
+	var save_dict = {
+		"transform": var_to_str(transform),
+		"age": age.age,
+		"turn": turn,
+		"warp_speed": warp_speed,
+	}
+	return save_dict
+
+
+func load_node(node_data):
+	#transform = str_to_var(node_data["transform"])
+	if "age" in node_data:
+		age.age = node_data.age
+	if "warp_speed" in node_data:
+		warp_speed = node_data.warp_speed
+	if "turn" in node_data:
+		turn = node_data.turn
