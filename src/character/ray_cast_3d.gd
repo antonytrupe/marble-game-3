dcb68cbed7ca6be0665ad6c699ed2eb857be9893
TonyT <antony.trupe@gmail.com>
2026-01-13 19:42:33 -0500

    feat: implement ray casting for highlighting colliding objects

---
+++ src/character/ray_cast_3d.gd
@@ -0,0 +1,34 @@
+extends RayCast3D
+
+var last_target: MeshInstance3D = null
+
+func _process(_delta):
+	if is_colliding():
+		var collider = get_collider()
+		# Find the MeshInstance3D child if the collider is a PhysicsBody
+		var mesh = collider.get_node_or_null("HighlightMeshInstance3D")
+
+		if mesh and mesh != last_target:
+			#print('colliding with raycast')
+			_clear_highlight()
+			_set_highlight(mesh, 1.0)
+			last_target = mesh
+		else:
+			_clear_highlight()
+	else:
+		_clear_highlight()
+
+
+func _set_highlight(mesh: MeshInstance3D, value: float):
+	#print('_set_highlight %s' % value)
+	# Use set_shader_parameter to toggle the effect
+	var mat = mesh.mesh.material as ShaderMaterial
+	#print(mat)
+	if mat:
+		mat.set_shader_parameter("intensity", value)
+
+
+func _clear_highlight():
+	if last_target:
+		_set_highlight(last_target, 0.0)
+		last_target = null
