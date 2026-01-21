493f88fb08a92822a6498daa448d02c55354f53f
TonyT <antony.trupe@gmail.com>
2026-01-20 22:10:31 -0500

    inventory ui stuff

---
+++ src/apple/apple_2d.gd
@@ -0,0 +1,50 @@
+class_name Apple2D
+extends RigidBody2D
+
+
+var is_dragging = false
+var is_dropping = false
+
+func _input(event: InputEvent):
+	# Stop dragging when the mouse button is released anywhere
+	if is_dragging and event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+			is_dragging = false
+
+
+func _on_input_event(viewport, event, shape_idx):
+	print('_on_input_event')
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed: # Mouse button down (Click/Start Drag)
+				is_dragging = true
+				# Set mode to static to stop physics, keep collision
+				self.freeze = false
+				# Calculate offset from center for smooth dragging
+				#mouse_offset = global_position - event.global_position
+				print('dragging')
+			else: # Mouse button up (Release)
+				print('stop dragging')
+				is_dragging = false
+				# Re-enable physics, body will sleep until next interaction
+				if not is_dropping:
+					self.freeze = true
+				# Optionally "throw" the object on release by applying a small impulse
+				# apply_central_impulse(Vector2(randf_range(-50, 50), randf_range(-50, 50))) # Example throw
+
+#func _process(delta):
+	#if is_dragging:
+		## Move body to mouse position plus the offset
+		#global_position = get_global_mouse_position() + mouse_offset
+
+func _physics_process(_delta):
+	if is_dragging:
+		var target_pos = get_global_mouse_position()
+		var direction = global_position.direction_to(target_pos)
+		var distance = global_position.distance_to(target_pos)
+		# Apply a force proportional to the distance to pull it toward the mouse
+		apply_central_force(direction * distance * 100)
+		# Add dampening to prevent orbiting the cursor
+		linear_velocity *= 0.9
+	elif not is_dropping:
+		linear_velocity = Vector2(0, 0)
--- src/inventory/inventory_window.gd
+++ src/inventory/inventory_window.gd
@@ -1,12 +1,27 @@
 class_name InventoryWindow
 extends MarbleWindow
 
-@onready var window: MarbleWindow = %Window
-
 @export var items: Inventory
 
+@onready var window: MarbleWindow = %Window
 
 func _on_client_current_character_updated(c: MarbleCharacter) -> void:
 	items = c.inventory
 	print(c.player_name)
 	print(c.inventory.items)
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	print('dropping')
+	#TODO drop the item on the group
+	#TODO remove the 2d object from the tree
+	if body is Apple2D:
+		body.gravity_scale = 1
+		body.freeze = false
+		body.is_dropping = true
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Apple2D:
+		body.gravity_scale = 0
+		body.is_dropping = false
