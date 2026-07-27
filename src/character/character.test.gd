0c190fbd991a5a1e67626fde4d908f60991c7e74
TonyT <antony.trupe@gmail.com>
2026-07-26 20:29:15 -0400

    Refactor MarbleCharacterTest to use instantiated scene and mock client, update test cases

--- src/character/character.test.gd
+++ src/character/character.test.gd
@@ -6,22 +6,46 @@ extends GdUnitTestSuite
 
 # TestSuite generated from
 const __source: String = 'res://src/character/character.gd'
+const MarbleCharacterScene: PackedScene = preload("res://src/character/character.tscn")
+const ClientScript: Script = preload("res://src/client/client.gd")
 
 
 var instance: MarbleCharacter
-var other: MarbleCharacter
+var _client_mock: GdUnitMock
 
 func before_test() -> void:
-	instance = MarbleCharacter.new()
-	other = MarbleCharacter.new()
+	instance = auto_free(MarbleCharacterScene.instantiate())
+	add_child(instance)
+	# Mock the client to avoid errors since it's accessed via an absolute path
+	_client_mock = mock(ClientScript).new()
+	var actions_mock: GdUnitMock = mock(Node).new()
+	_client_mock.add_property("actions", actions_mock)
+	# instance.client = _client_mock
 
 
 func after_test() -> void:
-	instance.queue_free()
-	other.queue_free()
+	# auto_free in before_test handles cleanup
+	pass
 
 
-func test_vector_to_array() -> void:
+func test_vector_to_string() -> void:
 	var v:Vector3 = Vector3(1, 2, 3)
 	var s:String = var_to_str(v)
-	assert_str(s).is_equal('"(1.0, 2.0, 3.0)"')
+	assert_str(s).is_equal('Vector3(1, 2, 3)')
+
+
+func test_initial_state() -> void:
+	assert_that(instance.standard_action).is_true()
+	assert_that(instance.mode).is_equal(MarbleCharacter.MODE.WALK)
+	assert_that(instance.actions).is_empty()
+
+
+func test_get_subject_verbs_returns_pick_up() -> void:
+	var verbs: Array[Callable] = instance.get_subject_verbs()
+	assert_that(verbs).has_size(1)
+	assert_that(verbs[0].get_method()).is_equal("pick_up")
+
+
+func test_get_object_verbs_returns_empty() -> void:
+	var verbs: Array[Callable] = instance.get_object_verbs([])
+	assert_that(verbs).is_empty()
