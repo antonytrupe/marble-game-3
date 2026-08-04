a71a42265c1e0be1b3b60feeff17b682df02e341
Tony T <atrupe@liberty.edu>
2026-08-03 22:01:07 -0400

    Add support for mocking return values in `GdUnitSpy`, enhance argument matching, and introduce return value preparation functionality

--- addons/gdUnit4/src/doubler/GdUnitSpyFunctionDoubler.gd
+++ addons/gdUnit4/src/doubler/GdUnitSpyFunctionDoubler.gd
@@ -4,5 +4,7 @@ extends GdFunctionDoubler
 
 func double(func_descriptor: GdFunctionDescriptor) -> PackedStringArray:
 	return GdUnitFunctionDoublerBuilder.new(func_descriptor)\
+		.with_prepare_block()\
 		.with_verify_block()\
+		.with_mocked_return_value()\
 		.build()
--- addons/gdUnit4/src/spy/GdUnitSpyImpl.gd
+++ addons/gdUnit4/src/spy/GdUnitSpyImpl.gd
@@ -7,6 +7,9 @@ class GdUnitSpyDoublerState:
 	const __SOURCE_CLASS := "{gdunit_source_class}"
 
 	var excluded_methods := PackedStringArray()
+	var is_prepare_return := false
+	var return_values := Dictionary()
+	var return_value: Variant = null
 
 	func _init(excluded_methods__ := PackedStringArray()) -> void:
 		excluded_methods = excluded_methods__
@@ -48,3 +51,90 @@ static func __get_verifier() -> GdUnitObjectInteractionsVerifier:
 static func __do_call_real_func(__func_name: String) -> bool:
 	@warning_ignore("unsafe_method_access")
 	return not __doubler_state().excluded_methods.has(__func_name)
+
+
+static func __is_prepare_return_value() -> bool:
+	var doubler_state := __doubler_state()
+	return doubler_state != null and doubler_state.is_prepare_return
+
+
+static func __sort_by_argument_matcher(__left_args: Array, __right_args: Array) -> bool:
+	for __index in __left_args.size():
+		var __larg: Variant = __left_args[__index]
+		if __larg is GdUnitArgumentMatcher:
+			return false
+	return true
+
+
+static func __sort_dictionary(__unsorted_args: Dictionary) -> Dictionary:
+	if __unsorted_args.size() <= 1:
+		return __unsorted_args
+	var __sorted_args: Array = __unsorted_args.keys()
+	__sorted_args.sort_custom(__sort_by_argument_matcher)
+	var __sorted_result := {}
+	for __index in __sorted_args.size():
+		var key :Variant = __sorted_args[__index]
+		__sorted_result[key] = __unsorted_args[key]
+	return __sorted_result
+
+
+static func __save_function_return_value(__func_name: String, __func_args: Array) -> void:
+	var doubler_state := __doubler_state()
+	var mocked_return_value_by_args: Dictionary = doubler_state.return_values.get(__func_name, {})
+	mocked_return_value_by_args[__func_args] = doubler_state.return_value
+	doubler_state.return_values[__func_name] = __sort_dictionary(mocked_return_value_by_args)
+	doubler_state.return_value = null
+	doubler_state.is_prepare_return = false
+
+
+static func __is_mocked_args_match(__func_args: Array, __mocked_args: Array) -> bool:
+	var __is_matching := false
+	for __index in __mocked_args.size():
+		var __fuction_args: Array = __mocked_args[__index]
+		if __func_args.size() != __fuction_args.size():
+			continue
+		__is_matching = true
+		for __arg_index in __func_args.size():
+			var __func_arg: Variant = __func_args[__arg_index]
+			var __mock_arg: Variant = __fuction_args[__arg_index]
+			if __mock_arg is GdUnitArgumentMatcher:
+				@warning_ignore("unsafe_method_access")
+				__is_matching = __is_matching and __mock_arg.is_match(__func_arg)
+			else:
+				__is_matching = __is_matching and typeof(__func_arg) == typeof(__mock_arg) and __func_arg == __mock_arg
+			if not __is_matching:
+				break
+		if __is_matching:
+			break
+	return __is_matching
+
+
+static func __return_mock_value(__func_name: String, __func_args: Array, __default_return_value: Variant) -> Variant:
+	var doubler_state := __doubler_state()
+	if not doubler_state.return_values.has(__func_name):
+		return __default_return_value
+	@warning_ignore("unsafe_method_access")
+	var __mocked_args: Array = doubler_state.return_values.get(__func_name).keys()
+	for __index in __mocked_args.size():
+		var __margs: Variant = __mocked_args[__index]
+		if __is_mocked_args_match(__func_args, [__margs]):
+			return doubler_state.return_values[__func_name][__margs]
+	return __default_return_value
+
+
+static func __is_do_not_call_real_func(__func_name: String, __func_args := []) -> bool:
+	var doubler_state := __doubler_state()
+	if doubler_state == null:
+		return false
+	# For spies, only skip the real function if a return value has been stubbed
+	if doubler_state.return_values.has(__func_name):
+		@warning_ignore("unsafe_method_access")
+		var __mocked_args: Array = doubler_state.return_values.get(__func_name).keys()
+		return __is_mocked_args_match(__func_args, __mocked_args)
+	return false
+
+
+func __do_return(mock_do_return_value: Variant) -> Object:
+	__doubler_state().return_value = mock_do_return_value
+	__doubler_state().is_prepare_return = true
+	return self
