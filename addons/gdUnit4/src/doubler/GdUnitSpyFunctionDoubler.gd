978b9c856d6f2266f37037b2346ae0987c6b8da0
Tony T <atrupe@liberty.edu>
2026-08-04 12:10:30 -0400

    Remove unused logic from `GdUnitSpy`, adjust texture imports, and rearrange enabled editor plugins for consistency.

--- addons/gdUnit4/src/doubler/GdUnitSpyFunctionDoubler.gd
+++ addons/gdUnit4/src/doubler/GdUnitSpyFunctionDoubler.gd
@@ -4,7 +4,5 @@ extends GdFunctionDoubler
 
 func double(func_descriptor: GdFunctionDescriptor) -> PackedStringArray:
 	return GdUnitFunctionDoublerBuilder.new(func_descriptor)\
-		.with_prepare_block()\
 		.with_verify_block()\
-		.with_mocked_return_value()\
 		.build()
