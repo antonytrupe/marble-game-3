b165d68c368b174032f2ddef88f7ba1b82e59465
TonyT <antony.trupe@gmail.com>
2026-08-07 22:20:53 -0400

    Clean up unnecessary whitespace in `tree.test.gd`.

--- src/tree/tree.test.gd
+++ src/tree/tree.test.gd
@@ -7,13 +7,13 @@ var instance: MarbleTree
 func before_test() -> void:
 	instance = mock(MarbleTree,CALL_REAL_FUNC)
 	#add_child(instance)
-	
-	
+
+
 func test_not_null() -> void:
 	assert_bool(true)
 	assert_that(instance).is_not_null()
-	
-	
+
+
 func test_mock_get_data()->void:
 	#instance=mock(instance)
 	do_return({"test":"test"}).on(instance).get_data()
